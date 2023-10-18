7151e95905d3f1f7d9cdf236ee5e6076b64eead3
Antonio Morales <126864866+Antoniomorales17@users.noreply.github.com>
2023-10-18 08:50:36 +0000

    test

--- blog/src/components/TopBar/TopBar.js
+++ blog/src/components/TopBar/TopBar.js
@@ -4,6 +4,7 @@ import "./TopBar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
+
 export default function Topbar() {
   const user = true; // Puedes ajustar esto según tu lógica de autenticación
 
